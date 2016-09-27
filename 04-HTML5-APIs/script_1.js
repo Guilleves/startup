a6ced471ee4fc78a93a3b446eb9d15e31a88720f
guillermina <guilleves@gmail.com>
2016-09-27 10:53:51 -0300

    [Guillermina Vescovo] - Exercise 3 - Complete T04E03, why is first file not read?

--- 04-HTML5-APIs/script_1.js
+++ 04-HTML5-APIs/script_1.js
@@ -15,3 +15,35 @@ function removeLastWill(){
     alert("having second thoughts huh");
     document.querySelector("p.last-will").innerHTML = "you greedy bastard";
 }
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    let files = evt.dataTransfer.files; // FileList object.
+    let output = [];
+    //console.log(files);
+
+    for (let i = 0; i < files.length; i++) {
+        var reader = new FileReader();
+        reader.onload = function(evt) {
+            //console.log(output);
+            output.push(reader.result);
+            //console.log(output);
+            document.getElementById("output").innerHTML += reader.result;
+        };
+        reader.readAsText(files[i]);
+        console.log(output[i]);
+    }
+}
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+}
+
+  // dnd listeners.
+  let dropZone = document.getElementById('drop-zone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
