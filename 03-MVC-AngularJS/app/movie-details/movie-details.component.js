325780d41a586fdc4bdf7f7adb79d8e86b1737e7
guillermina <guilleves@gmail.com>
2016-10-04 00:07:57 -0300

    [Guillermina Vescovo] - Exercise 2 - Complete T03E02, pimping for later

--- 03-MVC-AngularJS/app/movie-details/movie-details.component.js
+++ 03-MVC-AngularJS/app/movie-details/movie-details.component.js
@@ -1,9 +1,13 @@
 angular.module('movieDetails')
 .component('movieDetails', {
     templateUrl: "./movie-details/movie-details.template.html",
-    controller: ['$routeParams',
-        function MovieDetailsController($routeParams){
-            this.movieId = $routeParams.phoneId;
+    controller: ['$http', '$routeParams',
+        function MovieDetailsController($http, $routeParams){
+            $http.get('movies/' + $routeParams.movieId + ".json")
+                 .then(response => {
+                     this.movie = response.data;
+                 })
         }
+
     ]
 });
